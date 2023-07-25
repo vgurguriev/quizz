d6780bde7bbb74c6221500d99acbf215a1a49aa7
Veselin Gurguriev <veselin.gurguriev@sirma.bg>
2023-07-25 11:47:21 +0300

    Styled home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:quizz/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: StartScreen(),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 55, 12, 129),
+              Color.fromARGB(255, 48, 5, 70),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+              ),
+        ),
+        child: const StartScreen()
+        ),
     ),
   ));
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,36 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Text('Start Screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Let's Quizz",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quizz'),
+          ),
+        ],
+      ),
+    );
   }
 }
